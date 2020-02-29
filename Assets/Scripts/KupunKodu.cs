c6c2935ff1448a86b3dd78183da91b8f1a7ddee4
Cingozr <recaicingz@gmail.com>
2020-02-29 16:22:35 +0300

    Init Update

---
+++ Assets/Scripts/KupunKodu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KupunKodu : MonoBehaviour
+{
+    void Start()
+    {
+        //GetComponent<MeshRenderer>().enabled = false;
+        //GameObject.FindWithTag("KapsulTag").GetComponent<MeshRenderer>().enabled = false;
+        //var oyunObjeleri = GameObject.FindGameObjectsWithTag("OyunObjeleri");
+        //for (int i = 0; i < oyunObjeleri.Length; i++)
+        //{
+        //    oyunObjeleri[i].GetComponent<MeshRenderer>().enabled = false;
+        //}
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
